e3d877bf7e0da7c3d5c1c1c59cc2e90068b05090
nour <nour.3bas752005@gmail.com>
2026-05-21 00:32:38 +0300

    read & normalize data

--- src/data_loader.py
+++ src/data_loader.py
@@ -1,1 +1,25 @@
-# data_loader.py
+import pandas as pd
+import numpy as np
+
+
+def load_and_clean_data(file_path='data/AmesHousing.csv'):
+
+    df=pd.read_csv(file_path)
+    df_clean = df[['GrLivArea', 'SalePrice']].dropna()
+
+    x=df_clean['GrLivArea'].values
+    y=df_clean['SalePrice'].values #convert column into array
+    return x, y
+
+def normalization(x,y):
+    x_mean = np.mean(x)
+    x_std = np.std(x)
+
+    x_norm = (x - x_mean) / x_std
+
+    y_mean = np.mean(y)
+    y_std = np.std(y)
+
+    y_norm = (y - y_mean) / y_std
+
+    return x_norm, y_norm, x_mean, x_std, y_mean, y_std
